57963d44be7b114a96681110a73ddba872c85851
Rabeeh Khoury <rabeeh@solid-run.com>
2020-03-23 12:16:13 +0200

    refactor a009531, a008851 and a011270
    1. Add 'load conditional', 'jump condidional' and 'jump' to PBI
    instructions.
    2. Use SVR register to execute the PCIe workarounds on the relevant rev
    of the device.
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- rcw.py
+++ rcw.py
@@ -328,6 +328,34 @@ def build_pbi(lines):
             v2 = struct.pack(endianess + 'L', p2)
             subsection += v1
             subsection += v2
+        elif op == 'loadc':
+            if p1 == None or p2 == None:
+                print('Error: "loadc" instruction requires two parameters')
+                return ''
+            v1 = struct.pack(endianess + 'L', 0x80140000)
+            v2 = struct.pack(endianess + 'L', p1)
+            v3 = struct.pack(endianess + 'L', p2)
+            subsection += v1
+            subsection += v2
+            subsection += v3
+        elif op == 'jumpc':
+            if p1 == None or p2 == None:
+                print('Error: "jumpc" instruction requires two parameters')
+                return ''
+            v1 = struct.pack(endianess + 'L', 0x80850000)
+            v2 = struct.pack(endianess + 'L', p1)
+            v3 = struct.pack(endianess + 'L', p2)
+            subsection += v1
+            subsection += v2
+            subsection += v3
+        elif op == 'jump':
+            if p1 == None:
+                print('Error: "jump" instruction requires a parameter')
+                return ''
+            v1 = struct.pack(endianess + 'L', 0x80840000)
+            v2 = struct.pack(endianess + 'L', p1)
+            subsection += v1
+            subsection += v2
         elif op == 'awrite':
             if p1 == None or p2 == None:
                 print('Error: "awrite" instruction requires two parameters')
